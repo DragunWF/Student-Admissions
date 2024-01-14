d9e63c0e541d150d4ff7c3f5bdf49e72fbfe9268
dragunwf <plarisanmarc@gmail.com>
2024-01-14 18:55:54 +0800

    Update ApplyStudentForm.java

--- src/main/java/com/dragunwf/app/UserInterface/ApplyStudentForm.java
+++ src/main/java/com/dragunwf/app/UserInterface/ApplyStudentForm.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package com.dragunwf.app.UserInterface;
 
-/**
- *
- * @author Darth Wolf
- */
+import com.dragunwf.app.Misc.Data;
+
+
 public class ApplyStudentForm extends javax.swing.JFrame {
 
     /**
@@ -26,21 +21,132 @@ public class ApplyStudentForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        ApplyStudentPanel = new javax.swing.JPanel();
+        titleLabel = new javax.swing.JLabel();
+        subTitleLabel = new javax.swing.JLabel();
+        nameField = new javax.swing.JTextField();
+        GWAField = new javax.swing.JTextField();
+        submitButton = new javax.swing.JButton();
+        backButton = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        ApplyStudentPanel.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        titleLabel.setFont(new java.awt.Font("Segoe UI", 0, 22)); // NOI18N
+        titleLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        titleLabel.setText("Send a Student to Admissions");
+        titleLabel.setToolTipText("");
+
+        subTitleLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        subTitleLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        subTitleLabel.setText("Student Information");
+        subTitleLabel.setToolTipText("");
+
+        nameField.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        nameField.setText("Name");
+
+        GWAField.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        GWAField.setText("GWA");
+        GWAField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                GWAFieldActionPerformed(evt);
+            }
+        });
+
+        submitButton.setText("Submit");
+        submitButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                submitButtonActionPerformed(evt);
+            }
+        });
+
+        backButton.setText("Back");
+        backButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                backButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout ApplyStudentPanelLayout = new javax.swing.GroupLayout(ApplyStudentPanel);
+        ApplyStudentPanel.setLayout(ApplyStudentPanelLayout);
+        ApplyStudentPanelLayout.setHorizontalGroup(
+            ApplyStudentPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(ApplyStudentPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(ApplyStudentPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(titleLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 472, Short.MAX_VALUE)
+                    .addComponent(subTitleLabel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 472, Short.MAX_VALUE))
+                .addContainerGap())
+            .addGroup(ApplyStudentPanelLayout.createSequentialGroup()
+                .addGap(162, 162, 162)
+                .addGroup(ApplyStudentPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(nameField, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(ApplyStudentPanelLayout.createSequentialGroup()
+                        .addComponent(submitButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(backButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(GWAField, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        ApplyStudentPanelLayout.setVerticalGroup(
+            ApplyStudentPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(ApplyStudentPanelLayout.createSequentialGroup()
+                .addGap(87, 87, 87)
+                .addComponent(titleLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(subTitleLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(nameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(GWAField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(ApplyStudentPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(submitButton)
+                    .addComponent(backButton))
+                .addContainerGap(128, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(ApplyStudentPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(ApplyStudentPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void GWAFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_GWAFieldActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_GWAFieldActionPerformed
+
+    private void backButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_backButtonActionPerformed
+
+    private void submitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_submitButtonActionPerformed
+        try {
+            String studentName = nameField.getText();
+            String studentGWA = GWAField.getText();
+            System.out.printf("%s %s\n", studentName, studentGWA);
+            Data.addStudent(studentName, Double.parseDouble(studentGWA));
+            nameField.setText("Name");
+            GWAField.setText("GWA");
+        } catch (Exception error) {
+            GWAField.setText("Please set a valid GWA!");
+        }
+    }//GEN-LAST:event_submitButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -76,7 +182,18 @@ public class ApplyStudentForm extends javax.swing.JFrame {
             }
         });
     }
+    
+    public void start() {
+        main(new String[0]);
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel ApplyStudentPanel;
+    private javax.swing.JTextField GWAField;
+    private javax.swing.JButton backButton;
+    private javax.swing.JTextField nameField;
+    private javax.swing.JLabel subTitleLabel;
+    private javax.swing.JButton submitButton;
+    private javax.swing.JLabel titleLabel;
     // End of variables declaration//GEN-END:variables
 }
