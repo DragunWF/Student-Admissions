fef912c8591c8d6632e346ba7e514539ad6328b1
dragunwf <plarisanmarc@gmail.com>
2024-01-14 15:14:43 +0800

    Update Data.java

--- src/main/java/com/dragunwf/app/Misc/Data.java
+++ src/main/java/com/dragunwf/app/Misc/Data.java
@@ -1,5 +1,11 @@
 package com.dragunwf.app.Misc;
 
+import java.util.ArrayList;
+
 public class Data {
+    private static ArrayList<Student> students = new ArrayList<>();
     
+    public static ArrayList<Student> getStudents() {
+        return students;
+    }
 }
