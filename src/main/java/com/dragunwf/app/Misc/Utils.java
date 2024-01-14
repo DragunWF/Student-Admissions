b3a055a7e9f2195972349eef7aa62eb99481bdc8
dragunwf <plarisanmarc@gmail.com>
2024-01-14 21:37:31 +0800

    Update Utils.java

--- src/main/java/com/dragunwf/app/Misc/Utils.java
+++ src/main/java/com/dragunwf/app/Misc/Utils.java
@@ -1,5 +1,17 @@
 package com.dragunwf.app.Misc;
 
 public class Utils {
-    
+    public static boolean removeStudent(String name) {
+        if (Data.getStudents().contains(name)) {
+            for (int i = 0, n = Data.getStudents().size(); i < n; i++) {
+                String studentName = Data.getStudents().get(i).getName();
+                if (studentName.equalsIgnoreCase(name)) {
+                    Data.getStudents().remove(i);
+                    break;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
 }
