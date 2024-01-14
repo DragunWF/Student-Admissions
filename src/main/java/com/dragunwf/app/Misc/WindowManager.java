ec5d94bdf12d0ca5858de6df629d6bbd830bda54
dragunwf <plarisanmarc@gmail.com>
2024-01-14 20:09:45 +0800

    Update WindowManager.java

--- src/main/java/com/dragunwf/app/Misc/WindowManager.java
+++ src/main/java/com/dragunwf/app/Misc/WindowManager.java
@@ -9,16 +9,35 @@ import java.util.HashMap;
 
 public class WindowManager {
     private static HashMap<String, Boolean> windowsOpened;
+    private static IntroMenu introMenu;
     private static ApplyStudentForm applyStudentForm;
     private static StudentMenu studentMenu;
     private static ViewStudents viewStudents;
+    
+    public static void openIntroMenu() { open("introMenu"); }
+    public static void openApplyStudentForm() { open("applyStudentForm"); }
+    public static void openStudentMenu() { open("studentMenu"); }
+    public static void openViewStudents() { open("viewStudents"); }
+    
+    public static void initialize() {
+        windowsOpened = new HashMap<>();
+        windowsOpened.put("introMenu", false);
+        windowsOpened.put("applyStudentForm", false);
+        windowsOpened.put("studentMenu", false);
+        windowsOpened.put("viewStudents", false);
+    }
 
-    private static void openWindow(String name) throws Exception {
+    private static void open(String name) {
         if (windowsOpened.get(name)) {
             return; // avoids window duplication
         }
 
         switch (name) {
+            case "introMenu" -> { 
+                introMenu = new IntroMenu();
+                introMenu.setVisible(true);
+                introMenu.setResizable(false);
+            }
             case "applyStudentForm" -> {
                 applyStudentForm = new ApplyStudentForm();
                 applyStudentForm.setVisible(true);
@@ -34,7 +53,7 @@ public class WindowManager {
                 viewStudents.setVisible(true);
                 viewStudents.setResizable(false);
             }
-            default -> throw new Exception("Window name passed is not recognized!");
+            default -> System.out.println("Window name is not recognized!");
         }
 
         windowsOpened.put(name, true);
