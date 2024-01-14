3dd4a05d867af5403b3d5e5207bdab82f6c488f5
dragunwf <plarisanmarc@gmail.com>
2024-01-14 15:14:49 +0800

    Update Student.java

--- src/main/java/com/dragunwf/app/Misc/Student.java
+++ src/main/java/com/dragunwf/app/Misc/Student.java
@@ -1,5 +1,26 @@
 package com.dragunwf.app.Misc;
 
 public class Student {
+    private String name;
+    private double gwa;
     
+    public Student(String name, double gwa) {
+        this.name = name;
+        this.gwa = gwa;
+    }
+    
+    public String getName() { return name; }
+    public double getGWA() { return gwa; }
+    
+    public void setName(String name) { this.name = name; }
+    
+    public void setGWA(double gwa) {
+        if (gwa < 1.0) { 
+            this.gwa = 1.0;
+        } else if (gwa > 5.0) {
+            this.gwa = 5.0;
+        } else {
+            this.gwa = gwa;
+        }
+    }
 }
