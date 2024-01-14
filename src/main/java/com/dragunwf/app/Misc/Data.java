ae7804616b9fd27bac2b7fa374bdf871d772c7a6
dragunwf <plarisanmarc@gmail.com>
2024-01-14 18:26:02 +0800

    Update Data.java

--- src/main/java/com/dragunwf/app/Misc/Data.java
+++ src/main/java/com/dragunwf/app/Misc/Data.java
@@ -8,4 +8,18 @@ public class Data {
     public static ArrayList<Student> getStudents() {
         return students;
     }
+    
+    public static void addDummyData() {
+        Student[] dummyData = {
+            new Student("Jack", 2.25),
+            new Student("Faye", 1.5),
+            new Student("Brad", 3.0),
+            new Student("Jarvis", 1.75),
+            new Student("Marc", 1.0),
+            new Student("Scott", 1.25)
+        };
+        for (Student student : dummyData) {
+            students.add(student);
+        }
+    }
 }
