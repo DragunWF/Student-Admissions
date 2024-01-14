9ca84ef0fe0192f9a69fd0a2cb059fb47db34bb1
dragunwf <plarisanmarc@gmail.com>
2024-01-14 18:25:56 +0800

    Update App.java

--- src/main/java/com/dragunwf/app/App.java
+++ src/main/java/com/dragunwf/app/App.java
@@ -1,10 +1,13 @@
 package com.dragunwf.app;
 
 import com.dragunwf.app.UserInterface.IntroMenu;
+import com.dragunwf.app.Misc.Data;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Initializing App...");
+        System.out.println("Inserting data...");
+        Data.addDummyData();
+        System.out.println("Initializing App..."); 
         IntroMenu introMenu = new IntroMenu();
         introMenu.start();
     }
