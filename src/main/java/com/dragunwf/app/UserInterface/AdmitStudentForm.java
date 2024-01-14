c2de98a278390f0d9ec3d696471de8af07d59dae
dragunwf <plarisanmarc@gmail.com>
2024-01-14 20:46:59 +0800

    Update AdmitStudentForm.java

--- src/main/java/com/dragunwf/app/UserInterface/AdmitStudentForm.java
+++ src/main/java/com/dragunwf/app/UserInterface/AdmitStudentForm.java
@@ -8,12 +8,12 @@ package com.dragunwf.app.UserInterface;
  *
  * @author Darth Wolf
  */
-public class AdmitStudentMenu extends javax.swing.JFrame {
+public class AdmitStudentForm extends javax.swing.JFrame {
 
     /**
      * Creates new form AdmitStudentMenu
      */
-    public AdmitStudentMenu() {
+    public AdmitStudentForm() {
         initComponents();
     }
 
@@ -59,20 +59,21 @@ public class AdmitStudentMenu extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AdmitStudentMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdmitStudentForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AdmitStudentMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdmitStudentForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AdmitStudentMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdmitStudentForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AdmitStudentMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdmitStudentForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new AdmitStudentMenu().setVisible(true);
+                new AdmitStudentForm().setVisible(true);
             }
         });
     }
